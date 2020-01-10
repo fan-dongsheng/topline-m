6ae99995e1ea79f943326e6f315b444646656daf
Your Name <f137000360@qq.com>
2020-01-10 18:07:57 +0800

    发送 token--请求拦截设置

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 // 封装 axios 请求模块;
 import axios from 'axios'
 import jsonBig from 'json-bigint' // 处理后端返回数据超出安全整数范围;
+import store from '@/store' // store导出的是store实例组件,可以直接store.state获取  和this.$store一样
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
@@ -19,3 +20,31 @@ request.defaults.transformResponse = [function (data) {
     console.log('转换失败', error)
   }
 }]
+
+// 添加token请求拦截器;统一处理发送请求需要在headers中带token
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 配置config;将令牌token配置上
+  const { user } = store.state
+  if (user) {
+    // config.headers 获取操作请求头对象
+    // Authorization 是后端要求的字段名称
+    // 数据值后端要求提供：Bearer token数据
+    //    注意：Bearer 后面有个空格
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error)
+})
