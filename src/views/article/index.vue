0e61a0dbb695e44d5061dc78b94e79935fc30751
Your Name <f137000360@qq.com>
2020-01-16 20:02:34 +0800

    文章详情--页面布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,26 +1,196 @@
 <template>
   <div class="article-container">
-      文章详情
+      <!-- 导航栏 -->
+    <van-nav-bar
+      title="文章详情"
+      left-arrow
+      fixed
+      @click-left="$router.back()"
+    ></van-nav-bar>
+    <!-- /导航栏 -->
+
+    <!-- 加载中 -->
+    <van-loading
+      class="loading"
+      color="#1989fa"
+      vertical
+    >
+      <slot>加载中...</slot>
+    </van-loading>
+    <!-- /加载中 -->
+
+    <!-- 文章详情 -->
+    <div class="detail">
+      <h3 class="title">一个合格的中级前端工程师需要掌握的 28 个 JavaScript 技巧</h3>
+      <div class="author-wrap">
+        <div class="base-info">
+          <van-image
+            class="avatar"
+            round
+            fit="cover"
+            src="https://img.yzcdn.cn/vant/cat.jpeg"
+          />
+          <div class="text">
+            <p class="name">黑马头条号</p>
+            <p class="time">4 小时前</p>
+          </div>
+        </div>
+        <van-button class="follow-btn" type="info" size="small" round>+ 关注</van-button>
+      </div>
+      <div class="markdown-body">
+        <p>作为战斗在业务一线的前端，要想少加班，就要想办法提高工作效率。这里提一个小点，我们在业务开发过程中，经常会重复用到日期格式化、url参数转对象、浏览器类型判断、节流函数等一类函数，这些工具类函数，基本上在每个项目都会用到，为避免不同项目多次复制粘贴的麻烦，我们可以统一封装，发布到npm，以提高开发效率。</p>
+        <p>使用 Object.prototype.toString 配合闭包，通过传入不同的判断类型来返回不同的判断函数，一行代码，简洁优雅灵活（注意传入 type 参数时首字母大写）</p>
+        <p>使用 Object.prototype.toString 配合闭包，通过传入不同的判断类型来返回不同的判断函数，一行代码，简洁优雅灵活（注意传入 type 参数时首字母大写）</p>
+        <p>使用 Object.prototype.toString 配合闭包，通过传入不同的判断类型来返回不同的判断函数，一行代码，简洁优雅灵活（注意传入 type 参数时首字母大写）</p>
+        <p>使用 Object.prototype.toString 配合闭包，通过传入不同的判断类型来返回不同的判断函数，一行代码，简洁优雅灵活（注意传入 type 参数时首字母大写）</p>
+      </div>
+    </div>
+    <!-- /文章详情 -->
+
+    <!-- 加载失败提示 -->
+    <div class="error">
+      <img src="./no-network.png" alt="no-network">
+      <p class="text">亲，网络不给力哦~</p>
+      <van-button
+        class="btn"
+        type="default"
+        size="small"
+      >点击重试</van-button>
+    </div>
+    <!-- /加载失败提示 -->
+
+    <!-- 底部区域 -->
+    <div class="footer">
+      <van-button
+        class="write-btn"
+        type="default"
+        round
+        size="small"
+      >写评论</van-button>
+      <van-icon
+        class="comment-icon"
+        name="comment-o"
+        info="9"
+      />
+      <van-icon
+        color="orange"
+        name="star"
+      />
+      <van-icon
+        color="#e5645f"
+        name="good-job"
+      />
+      <van-icon class="share-icon" name="share" />
+    </div>
+    <!-- /底部区域 -->
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-
-    }
-  },
+  name: 'ArticlePage',
+  components: {},
   props: {
     articleId: {
       type: String,
       required: true
     }
-  }
-
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+@import "./article.css";
+
+.article-container {
+  padding: 46px 20px 50px;
+  background: #fff;
+  .loading {
+    padding-top: 100px;
+    text-align: center;
+  }
+  .detail {
+    .title {
+      margin: 0;
+      padding-top: 24px;
+      font-size: 20px;
+      color: #3A3A3A;
+    }
+    .author-wrap {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 20px 0;
+      height: 40px;
+      .base-info {
+        display: flex;
+        align-items: center;
+        .avatar {
+          width: 40px;
+          height: 40px;
+          margin-right: 8px;
+        }
+        .text {
+          line-height: 1.5;
+          .name {
+            margin: 0;
+            font-size: 14px;
+          }
+          .time {
+            margin: 0;
+            font-size: 12px;
+            color: #999;
+          }
+        }
+      }
+      .follow-btn {
+        width: 85px;
+      }
+    }
+  }
+  .error {
+    padding-top: 100px;
+    text-align: center;
+    .text {
+      font-size: 15px;
+    }
+    .btn {
+      width: 30%;
+    }
+  }
+  .footer {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 44px;
+    border-top: 1px solid #d8d8d8;
+    background-color: #fff;
+    .write-btn {
+      width: 160px;
+    }
+    .van-icon {
+      font-size: 20px;
+    }
+    .comment-icon {
+      bottom: -2px;
+    }
+    .share-icon {
+      bottom: -2px;
+    }
+  }
+}
 
 </style>
